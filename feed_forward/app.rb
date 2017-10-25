97073eb8ccca82300557ca5762ee7df6b066c827
vildbas <emil@vildbas.com>
2017-10-25 16:33:01 +0200

    Add files via upload
    Från mac, snart så

--- feed_forward/app.rb
+++ feed_forward/app.rb
@@ -9,4 +9,14 @@ get('/') do
     erb(:index, locals:{kurser:result})
 end
 
+#Hämta alla kommentarer från colllection
+
+get('/comments/:collectionid') do
+    collection_id = params[:collectionid]
+    db = SQLite3::Database.new('db/fast_forward_db.sqlite')
+    result = db.execute("SELECT text FROM commments WHERE collection_id IN (SELECT id FROM collections WHERE id = ?)",collection_id).first
+    print "RESULT::"
+    p result
+end
+
 
