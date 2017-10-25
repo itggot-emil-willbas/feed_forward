64e86d534cbf09fb56a7e3b211c710acfdd1e97b
vildbas <emil@vildbas.com>
2017-10-25 12:33:32 +0200

    Rätt nu

--- feed_forward/app.rb
+++ feed_forward/app.rb
@@ -1,3 +1,12 @@
-include 'sinatra'
-include 'sqlite3'
+require 'sinatra'
+require 'sqlite3'
+
+get('/') do
+
+    db = SQLite3::Database.new('db/fast_forward_db.sqlite')
+    result = db.execute("SELECT name FROM courses")
+    result = result[0..-1]
+    erb(:index, locals:{kurser:result})
+end
+
 
